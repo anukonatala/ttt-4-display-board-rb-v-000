f8098f7e35b6f27845dec0007b2cb158b6fbc18b
Anusha Konatala <anu.konatala@gmail.com>
2018-05-23 06:53:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,8 @@
 # out the current state.
 
 def display_board(board)
-  
+  puts "#{board[0]|#{board[1]|#{board[2]-----------#{board[3]|#{board[4]|#{board[5]
 end
+
+board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
